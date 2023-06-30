dcae1dc6dacf02a15205557fabee28a472612164
Dax Mickelson <dmickelson@zscaler.com>
2023-06-30 06:49:20 -0600

    set logging to file as optional

--- zscaler_api_talkers/zscaler_helpers/logger.py
+++ zscaler_api_talkers/zscaler_helpers/logger.py
@@ -6,7 +6,7 @@ from logging.handlers import RotatingFileHandler
 def setup_logger(
     name: str,
     level: str = os.getenv("LOGGING_LEVEL", "info"),
-    log_filename: str = "output.log",
+    log_filename: str = None,
     max_log_size: int = 10 * 1024 * 1024,  # 10 MB
     backup_count: int = 3,
 ) -> logging.Logger:
@@ -16,30 +16,33 @@ def setup_logger(
     Args:
         name (str): Name of logger
         level (str): os.getenv("LOGGING_LEVEL"). Defaults to 'info'.
-        log_filename (str): Name and Path of log file.  Defaults to "output.log"
+        log_filename (str): Name and Path of log file.
         max_log_size (int): Defaults to 10MB
         backup_count (int): Default to 3
 
     Return:
         Logger Object
     """
-    handler_stream = logging.StreamHandler()
-    handler_file = RotatingFileHandler(
-        log_filename,
-        maxBytes=max_log_size,
-        backupCount=backup_count,
-    )
+    logger = logging.getLogger(name)
+
     formatter = logging.Formatter(
         fmt="{asctime} {name}.{funcName} {levelname} {message}",
         datefmt="%Y%m%d %H:%M:%S",
         style="{",
     )
-    handler_stream.setFormatter(formatter)
-    handler_file.setFormatter(formatter)
 
-    logger = logging.getLogger(name)
+    if log_filename:
+        handler_file = RotatingFileHandler(
+            log_filename,
+            maxBytes=max_log_size,
+            backupCount=backup_count,
+        )
+        handler_file.setFormatter(formatter)
+        logger.addHandler(handler_file)
+
+    handler_stream = logging.StreamHandler()
+    handler_stream.setFormatter(formatter)
     logger.addHandler(handler_stream)
-    logger.addHandler(handler_file)
 
     log_level = logging.INFO
     level = level.lower()
